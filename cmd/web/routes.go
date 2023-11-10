6846510c86ea05138105aad659910ba71a33108e
septicalls <septicalls@proton.me>
2023-11-10 23:41:52 +0530

    defined a route for viewing a placement drive

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,7 +9,12 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFount(w)
+	})
+
 	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/drive/:id", app.driveView)
 
 	return router
 }
