cff041dfb98d4942002f9786d7d0a2e7ff7f9265
septicalls <septicalls@proton.me>
2023-11-11 11:57:38 +0530

    executed templates in a two-stage process to catch runtime errors

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -29,10 +30,14 @@ func (app *application) render(w http.ResponseWriter, status int, page string) {
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", nil)
+	err := ts.ExecuteTemplate(buf, "base", nil)
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	w.WriteHeader(status)
+	buf.WriteTo(w)
 }
