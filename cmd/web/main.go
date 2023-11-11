75b795bdeaeed34772faac27bee81a51e8e0c0fc
septicalls <septicalls@proton.me>
2023-11-11 11:13:42 +0530

    separated log declarations

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -25,9 +25,12 @@ func main() {
 
 	flag.Parse()
 
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
 	app := &application{
-		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
-		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		errorLog: errorLog,
+		infoLog:  infoLog,
 	}
 
 	app.infoLog.Printf("Starting server on %s", cfg.addr)
