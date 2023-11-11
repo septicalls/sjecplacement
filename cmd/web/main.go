3bdff28fcf1f1b01e128f41ba91a0eb0406f2a51
septicalls <septicalls@proton.me>
2023-11-11 14:59:47 +0530

    connected to DB from code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
 type config struct {
 	addr      string
 	staticDir string
+	dsn       string
 }
 
 type application struct {
@@ -22,6 +27,9 @@ type application struct {
 func main() {
 	var cfg config
 
+	dsn := "postgres://default:%s@ep-super-pond-22168364-pooler.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb"
+	pass := flag.String("pass", "[REDACTED]", "Password for the PostgreSQL Database")
+
 	flag.StringVar(&cfg.addr, "addr", ":8080", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 
@@ -29,6 +37,20 @@ func main() {
 
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	if *pass == "[REDACTED]" {
+		flag.Usage()
+		errorLog.Fatal("Please provide a password for the DB")
+	}
+
+	cfg.dsn = fmt.Sprintf(dsn, *pass)
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	templateCache, err := newTemplateCache()
 	if err != nil {
@@ -50,4 +72,17 @@ func main() {
 	app.infoLog.Printf("Starting server on %s", cfg.addr)
 	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
