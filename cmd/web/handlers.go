f0445976106ae7a73ef7c7948ddb7bea64f26d1e
septicalls <septicalls@proton.me>
2023-11-12 12:50:40 +0530

    hardcoded another sample drive for development

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -56,10 +56,10 @@ func (app *application) driveCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) driveCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "The sceptix club recruitement"
-	company := "The sceptix club"
-	description := `LOREM IPSUM DOLOR AMET`
-	date := time.Now().Truncate(24 * time.Hour)
+	title := "GDSC Web Dev Project Screening"
+	company := "Google Developer Student Club - SJEC"
+	description := `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae proin sagittis nisl rhoncus. In mollis nunc sed id semper. Enim diam vulputate ut pharetra. Diam vel quam elementum pulvinar etiam non quam lacus suspendisse. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Netus et malesuada fames ac turpis egestas sed tempus urna. Curabitur gravida arcu ac tortor. In hendrerit gravida rutrum quisque non tellus orci. Elementum sagittis vitae et leo duis ut. Nisl purus in mollis nunc sed id semper risus in. Feugiat scelerisque varius morbi enim nunc faucibus. Mattis vulputate enim nulla aliquet. Tincidunt arcu non sodales neque. Proin sagittis nisl rhoncus mattis rhoncus.`
+	date := time.Now().Truncate(24*time.Hour).AddDate(0, 0, 365)
 
 	id, err := app.drives.Insert(title, company, description, date)
 	if err != nil {
