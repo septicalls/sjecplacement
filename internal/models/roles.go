8c238ba94f15c26cd7c17e098e417e788311f0c5
septicalls <septicalls@proton.me>
2023-11-13 22:43:07 +0530

    defined a method to insert roles

--- internal/models/roles.go
+++ internal/models/roles.go
@@ -21,6 +21,20 @@ type RoleModel struct {
 	DB *sql.DB
 }
 
+func (m *RoleModel) Insert(r *Role) (int, error) {
+	stmt := `INSERT INTO roles (profile, description, qualification, cutoff, location, stipend, ctc,
+	serviceagreement, drive_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`
+
+	var id int
+	err := m.DB.QueryRow(stmt, r.Profile, r.Description, r.Qualification, r.Cutoff, r.Location,
+		r.Stipend, r.CTC, r.ServiceAgreement, r.DriveID).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
 func (m *RoleModel) All(id int) ([]*Role, error) {
 	stmt := `SELECT id, profile, description, qualification, cutoff, location, stiped, ctc,
 	serviceagreement FROM roles WHERE id = $1 ORDER BY id ASC`
