76701681f4df39ba2b944bbbd21a1c3c860c4939
septicalls <septicalls@proton.me>
2023-11-13 15:26:53 +0530

    defined a roles model

---
+++ internal/models/roles.go
@@ -0,0 +1,65 @@
+package models
+
+import "database/sql"
+
+type Role struct {
+	ID            int
+	Profile       string
+	Description   string
+	Qualification string
+	// Nullable
+	Cutoff           sql.NullString
+	Location         sql.NullString
+	Stipend          sql.NullInt32
+	CTC              sql.NullFloat64
+	ServiceAgreement sql.NullFloat64
+	// Reference to Drive Model
+	DriveID int
+}
+
+type RoleModel struct {
+	DB *sql.DB
+}
+
+func (m *RoleModel) All(id int) ([]*Role, error) {
+	stmt := `SELECT id, profile, description, qualification, cutoff, location, stiped, ctc,
+	serviceagreement FROM roles WHERE id = $1 ORDER BY id ASC`
+
+	rows, err := m.DB.Query(stmt, id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	roles := []*Role{}
+
+	for rows.Next() {
+		r := &Role{}
+
+		err := rows.Scan(
+			&r.ID,
+			&r.Profile,
+			&r.Description,
+			&r.Qualification,
+			&r.Cutoff,
+			&r.Location,
+			&r.Stipend,
+			&r.CTC,
+			&r.ServiceAgreement,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		r.DriveID = id
+
+		roles = append(roles, r)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return roles, nil
+}
